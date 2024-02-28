6c991ced457483fce6db353d6d30f7544619363d
Vitalii Pilikin <oversea@bk.ru>
2024-02-28 23:35:33 +0200

    new pass

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,84 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client'
+
+import { useEffect, useState } from 'react'
+import { Container, Pagination } from 'react-bootstrap'
+import './style.css'
 
 export default function Home() {
+  const screenWidth = window.screen.width
+  const [num, setNum] = useState([])
+  const [result, setResult] = useState([])
+  const numLength = screenWidth < 768 ? 10 ** 4 : 10 ** 10
+
+  useEffect(() => {
+    setNum(() => getRandomNumber(numLength))
+    const onKeypress = (e) => {
+      let l = e.key
+      if (l >= 0) {
+        setResult((result) => [...result, l])
+      }
+      if (
+        l === 'ContextMenu' ||
+        l === ' ' ||
+        l === 'Backspace' ||
+        l === '§' ||
+        l === '>' ||
+        l === 'Enter'
+      ) {
+        setNum(() => getRandomNumber(numLength))
+        setResult([])
+      }
+    }
+
+    document.addEventListener('keypress', onKeypress)
+
+    return () => {
+      document.removeEventListener('keypress', onKeypress)
+    }
+  }, [])
+
+  const validate = result.map((el, i) => (el == num[i] ? true : false))
+
+  const getRandomNumber = (n) => {
+    const random = Math.floor(Math.random() * n)
+      .toString()
+      .split('')
+    while (random.length < 4) {
+      random.unshift('0')
+    }
+    return random
+  }
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <Container
+      onClick={() => {
+        setNum(getRandomNumber(numLength))
+        setResult([])
+      }}
+      className="mt-5 d-flex flex-column align-items-center "
+    >
+      <Container
+        onClick={() => {
+          setNum(getRandomNumber(numLength))
+          setResult([])
+        }}
+        className="d-block d-sm-none"
+        fluid
+      >
+        <input type="tel" value="" className="input-for-mobile"></input>
+      </Container>
+      <Pagination size="lg">
+        {num.map((el, i) => (
+          <Pagination.Item
+            key={i}
+            className={`big ${
+              validate[i] ? 'green' : validate[i] === false ? 'red' : ''
+            }`}
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+            {el}
+          </Pagination.Item>
+        ))}
+      </Pagination>
+    </Container>
+  )
 }
